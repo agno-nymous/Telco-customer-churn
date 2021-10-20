98c864f194c51b8a65ecdfb8ab48c02e5ff6798e
ashishthomaschempolil <ashishthomas7@gmail.com>
2021-10-20 17:06:59 -0400

    changed label and legends on app.py

--- app.py
+++ app.py
@@ -36,37 +36,35 @@ def st_shap(plot, height=None):
 #create a button to upload the test file
 uploaded_file = st.file_uploader("Upload your test csv file", type=["csv"])
 if uploaded_file:
-    test_df = pd.read_csv(uploaded_file)#.sort_values(by=['customerID'])
-    response_enc = resp_enc.response_encoding()
-    response_enc = joblib.load('./model/resp_enc.pkl')
-    test_df = response_enc.transform(test_df)
-    features = get_features(load_model())
-    x_test = test_df.loc[:, features]
-    y_pred_proba = load_model().predict_proba(x_test)[:,1]
-    y_pred = load_model().predict(x_test)
+    test_df = pd.read_csv(uploaded_file)#.sort_values(by=['customerID']) #reading the csv uploaded csv file
+    response_enc = resp_enc.response_encoding() #creating an object of the response encoding class
+    response_enc = joblib.load('./model/resp_enc.pkl') #loading the response encoding model
+    test_df = response_enc.transform(test_df) #transforming the test dataframe
+    features = get_features(load_model()) #getting the features of the saved model
+    x_test = test_df.loc[:, features] #creating a test dataframe with the features
+    y_pred_proba = load_model().predict_proba(x_test)[:,1] #predicting the probabilities of the test dataframe
+    y_pred = load_model().predict(x_test) #predicting the response of the test dataframe
 
 
 
     #plotting shap force plot
-    explainer, shap_values = get_shap(x_test)
+    explainer, shap_values = get_shap(x_test) #getting the explainer and shap values
     # st.write()
 
     id = st.selectbox('Select a customer id to analyse',test_df.customerID)
     st.write(
-        f""" ### Shapley force plot
-        Customer ID: {id}
-
-        Red: Increases probability of churn
-        Blue: Decreases probability of churn
-        length: strength of influence of feature on churn probability
+        f"""
+        ### Shapley force plot
+        #### Customer ID: {id}
+        ##### Legend:
+        * Red: Increases probability of churn
+        * Blue: Decreases probability of churn
+        * length: strength of influence of feature on churn probability
+        * Top Numbers are probability (Bold one is the probability of churn)  
+        * Bottom Numbers are feature values
         """
     )
-    st.write(
-        """
-        `* Top Numbers are probability (Bold one is the probability of churn)`
-        
-        `* Bottom Numbers are feature values`
-        """)
+
     index = test_df.query('customerID == @id').index[0]
     # plot = show_force_plot(index, shap_values, explainer)
     st_shap(
